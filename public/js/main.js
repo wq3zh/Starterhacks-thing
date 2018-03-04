4abfc3b38aa9e74dea3f87c7c14d3488958a8357
Kevin Jiang <kevin51jiang@email.com>
2018-03-04 08:24:51 -0500

    Idk what's even happening

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,26 @@
+var isOnXchange = false;
+
 $(document).ready(() => {
 
   // Place JavaScript code here... 
   if (document.getElementsByName('xchange-box')[0] != null) {
+    isOnXchange = true;
+    /*
     setInterval(() => {
-      console.log('refresh');
       document.getElementsByName('xchange-box')[0].src += '';
     }, 10000);
+    */
+  }
+});
+
+/**
+ * For refresh on enter (?)
+ */
+$(document).keypress((e) => {
+  if (isOnXchange === true) {
+    if (e.which == 13) {
+      //refresh
+      document.getElementsByName('xchange-box')[0].src += '';
+    }
   }
 });
